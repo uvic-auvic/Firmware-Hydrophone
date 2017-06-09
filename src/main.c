82e00c50dd86af2f392fbba198e45cf4a0d01c29
gmpgreen <gmpgreen16@gmail.com>
2017-06-09 15:24:57 -0700

    DMA working
    DMA and ADC tested with debug

--- src/main.c
+++ src/main.c
@@ -9,15 +9,13 @@
 */
 
 #include "stm32f4xx.h"
+#include "stm32f4xx_adc.h"
 //#include "usart.h"
 #include "main.h"
-#define ARRAYSIZE 8*4
-#define ADC1_DR    ((uint32_t)0x4001244C)
+#define ARRAYSIZE 8
 volatile uint16_t ADC_values[ARRAYSIZE];
 volatile uint32_t status = 0;
 			
-
-
 
 void ADCInit(void);
 void DMAInit(void);
@@ -30,12 +28,12 @@ ADCInit();
 DMAInit();
 
 //Enable DMA2 Channel transfer
-DMA_Cmd(DMA2_Channel1, ENABLE);
+//DMA_Cmd(DMA2_Channel1, ENABLE);
 //Start ADC1 Software Conversion
-ADC_SoftwareStartConvCmd(ADC1, ENABLE);
+ADC_SoftwareStartConv(ADC1);
 //wait for DMA complete
-while (!status){};
-ADC_SoftwareStartConvCmd(ADC1, DISABLE);
+//while (!status){};
+//ADC_SoftwareStartConvCmd(ADC1, DISABLE);
 //print averages
 /*for(index = 0; index<8; index++)
 	{
@@ -47,15 +45,17 @@ for(index = 0; index<8; index++){
 					+ADC_values[index+16]+ADC_values[index+24])/4));
 }
 
+
 while (1)
   {
-	//interrupts does the job
+	//interrupts
   }
 }
 
 void ADCInit(void){
-	//--Enable ADC1 and GPIOA--
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1 | RCC_APB2Periph_GPIOA, ENABLE);
+	/*enable peripheral clock for GPIOA */
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
 	GPIO_InitTypeDef GPIO_InitStructure; //Variable used to setup the GPIO pins
 	//==Configure ADC pins (PA0 -> Channel 0 to PA7 -> Channel 7) as analog inputs==
 	GPIO_StructInit(&GPIO_InitStructure); // Reset init structure, if not it can cause issues...
@@ -63,56 +63,73 @@ void ADCInit(void){
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
+	/*enable peripheral clock for ADC1 */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE); //double check!
+
 	ADC_InitTypeDef ADC_InitStructure;
 	//ADC1 configuration
+
 	//select continuous conversion mode
-	ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
+	//ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
+
 	//We will convert multiple channels
 	ADC_InitStructure.ADC_ScanConvMode = ENABLE;
 	//we will convert one time
 	ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;//!
 	//select no external triggering
-	ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+	ADC_InitStructure.ADC_ExternalTrigConvEdge = 0;
+	ADC_InitStructure.ADC_ExternalTrigConv = 0;
 	//right 12-bit data alignment in ADC data register
 	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
-	//8 channels conversion
-	ADC_InitStructure.ADC_NbrOfChannel = 8;
+	//8 ADC conversions for sequencer
+	ADC_InitStructure.ADC_NbrOfConversion = 8;
+
 	//load structure values to control and status registers
 	ADC_Init(ADC1, &ADC_InitStructure);
 	//wake up temperature sensor
 	//ADC_TempSensorVrefintCmd(ENABLE);
 	//configure each channel
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 1, ADC_SampleTime_41Cycles5);
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_1, 2, ADC_SampleTime_41Cycles5);
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_2, 3, ADC_SampleTime_41Cycles5);
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_3, 4, ADC_SampleTime_41Cycles5);
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_4, 5, ADC_SampleTime_41Cycles5);
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_5, 6, ADC_SampleTime_41Cycles5);
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_5, 7, ADC_SampleTime_41Cycles5);
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_7, 8, ADC_SampleTime_41Cycles5);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 1, ADC_SampleTime_56Cycles);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_1, 2, ADC_SampleTime_56Cycles);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_2, 3, ADC_SampleTime_56Cycles);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_3, 4, ADC_SampleTime_56Cycles);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_4, 5, ADC_SampleTime_56Cycles);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_5, 6, ADC_SampleTime_56Cycles);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_6, 7, ADC_SampleTime_56Cycles);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_7, 8, ADC_SampleTime_56Cycles);
+
+	ADC_CommonInitTypeDef ADC_CommonInitStructure;
+	ADC_CommonInitStructure.ADC_Mode = ADC_Mode_Independent;
+	ADC_CommonInitStructure.ADC_Prescaler = ADC_Prescaler_Div2; //appropriate speed?
+	ADC_CommonInitStructure.ADC_DMAAccessMode = ADC_DMAAccessMode_1;
+	ADC_CommonInitStructure.ADC_TwoSamplingDelay = ADC_TwoSamplingDelay_8Cycles;
+	ADC_CommonInit(&ADC_CommonInitStructure);
+
+	//Enables or disables the ADC DMA request after last transfer (Single-ADC mode)
+	ADC_DMARequestAfterLastTransferCmd(ADC1, ENABLE);
 	//Enable ADC1
+
 	ADC_Cmd(ADC1, ENABLE);
 	//enable DMA for ADC
 	ADC_DMACmd(ADC1, ENABLE);
-	//Enable ADC1 reset calibration register
-	ADC_ResetCalibration(ADC1);
-	//Check the end of ADC1 reset calibration register
-	while(ADC_GetResetCalibrationStatus(ADC1));
-	//Start ADC1 calibration
-	ADC_StartCalibration(ADC1);
-	//Check the end of ADC1 calibration
-	while(ADC_GetCalibrationStatus(ADC1));
+	//Any Calibation needed?
 }
 void DMAInit(void){
 	//enable DMA2 clock
-	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA2, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2, ENABLE);
 	//create DMA structure
 	DMA_InitTypeDef  DMA_InitStructure;
 	//reset DMA2 channe1 to default values;
-	DMA_DeInit(DMA2_Channel1);
+	DMA_DeInit(DMA2_Stream0);
 	//channel will be used for memory to memory transfer
-	DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
-	//setting normal mode (non circular)
+
+	DMA_InitStructure.DMA_Channel = DMA_Channel_0;
+	DMA_InitStructure.DMA_FIFOMode = DMA_FIFOMode_Disable;
+	DMA_InitStructure.DMA_FIFOThreshold = DMA_FIFOThreshold_HalfFull;
+	DMA_InitStructure.DMA_MemoryBurst = DMA_MemoryBurst_Single;
+	DMA_InitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
+
+	//setting circular mode
 	DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
 	//medium priority
 	DMA_InitStructure.DMA_Priority = DMA_Priority_High;
@@ -124,20 +141,21 @@ void DMAInit(void){
 	//source address increment disable
 	DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
 	//Location assigned to peripheral register will be source
-	DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;
+	DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralToMemory;
 	//chunk of data to be transfered
 	DMA_InitStructure.DMA_BufferSize = ARRAYSIZE;
 	//source and destination start addresses
-	DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)ADC1_DR;
-	DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)ADC_values;
+	DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&ADC1->DR;
+	DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t)&ADC_values[0]; //[0] ?
 	//send values to DMA registers
-	DMA_Init(DMA2_Channel1, &DMA_InitStructure);
+	DMA_Init(DMA2_Stream0, &DMA_InitStructure);
 	// Enable DMA2 Channel Transfer Complete interrupt
-	DMA_ITConfig(DMA2_Channel1, DMA_IT_TC, ENABLE);
-	DMA_Cmd(DMA2_Channel1, ENABLE); //Enable the DMA2 - Channel1
+	DMA_ITConfig(DMA2_Stream0, DMA_IT_TC, ENABLE);
+	DMA_Cmd(DMA2_Stream0, ENABLE); //Enable the DMA2 - Stream 0
+
 	NVIC_InitTypeDef NVIC_InitStructure;
 	//Enable DMA2 channel IRQ Channel */
-	NVIC_InitStructure.NVIC_IRQChannel = DMA2_Channel1_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannel = DMA2_Stream0_IRQn;
 	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
 	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
