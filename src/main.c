31cca32b061bbc32f6a05ea9b61e3bd45291730e
gmpgreen <gmpgreen16@gmail.com>
2017-06-13 17:10:04 -0700

    Initialized UART/DMA with std-periph-lib

--- src/main.c
+++ src/main.c
@@ -10,37 +10,41 @@
 
 #include "stm32f4xx.h"
 #include "stm32f4xx_adc.h"
-//#include "usart.h"
+#include "stm32f4xx_usart.h"
 #include "main.h"
 #define ARRAYSIZE 8
+#define BaudRate 9600
 volatile uint16_t ADC_values[ARRAYSIZE];
 
 void ADCInit(void);
-void DMAInit(void);
+void DMA2Init(void); //peripheral to memory
+void NVICInit(void);
+void UARTInit(void);
+void DMA1Init(void); //memory to peripheral
+void GPIOInit(void);
+
 int main(void)
 {
-uint8_t index;
-//initialize USART1
-//Usart1Init();
+
+GPIOInit();
 ADCInit();
-DMAInit();
-
-
+DMA2Init();
+NVICInit();
+UARTInit();
+DMA1Init();
 //Start ADC1 Software Conversion
 ADC_SoftwareStartConv(ADC1);
 
 
 while (1)
   {
-	for(index = 0; index<8; index++)
-		{
-		printf("ch%d = %d ",index, ADC_values[index]);
-		}
+
   }
 }
-
-void ADCInit(void){
-	/*enable peripheral clock for GPIOA */
+void GPIOInit(void){
+	/**
+	* Configuring GPIOA for ADC
+	*/
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
 
 	GPIO_InitTypeDef GPIO_InitStructure; //Variable used to setup the GPIO pins
@@ -48,8 +52,24 @@ void ADCInit(void){
 	GPIO_StructInit(&GPIO_InitStructure); // Reset init structure, if not it can cause issues...
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1| GPIO_Pin_2| GPIO_Pin_3| GPIO_Pin_4| GPIO_Pin_5| GPIO_Pin_6| GPIO_Pin_7;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
+	/**
+	* Configuring GPIOA for USART
+	*/
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART2);
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART2);
+	// Initialize pins as alternating function
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+}
+
+void ADCInit(void){
 	/*enable peripheral clock for ADC1 */
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
 
@@ -98,20 +118,18 @@ void ADCInit(void){
 	ADC_Cmd(ADC1, ENABLE);
 	//enable DMA for ADC
 	ADC_DMACmd(ADC1, ENABLE);
-	//Any Calibation needed?
 }
-void DMAInit(void){
+void DMA2Init(void){
 	//enable DMA2 clock
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2, ENABLE);
 	//create DMA structure
 	DMA_InitTypeDef  DMA_InitStructure;
 	//reset DMA2 channe1 to default values;
 	DMA_DeInit(DMA2_Stream0);
-	//channel will be used for memory to memory transfer
 
 	DMA_InitStructure.DMA_Channel = DMA_Channel_0;
-	DMA_InitStructure.DMA_FIFOMode = DMA_FIFOMode_Disable;
-	DMA_InitStructure.DMA_FIFOThreshold = DMA_FIFOThreshold_HalfFull;
+	DMA_InitStructure.DMA_FIFOMode = DMA_FIFOMode_Disable;  //not using FIFO
+	DMA_InitStructure.DMA_FIFOThreshold = DMA_FIFOThreshold_HalfFull; //not using FIFO
 	DMA_InitStructure.DMA_MemoryBurst = DMA_MemoryBurst_Single;
 	DMA_InitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
 
@@ -138,7 +156,48 @@ void DMAInit(void){
 	// Enable DMA2 Channel Transfer Complete interrupt
 	DMA_ITConfig(DMA2_Stream0, DMA_IT_TC, ENABLE);
 	DMA_Cmd(DMA2_Stream0, ENABLE); //Enable the DMA2 - Stream 0
+}
 
+void DMA1Init(void) {
+	//enable DMA2 clock
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA1, ENABLE);
+	//create DMA structure
+	DMA_InitTypeDef  DMA_InitStructure;
+	//reset DMA2 channel to default values;
+	DMA_DeInit(DMA1_Stream6); //stream 6 for usart2 tx
+
+	DMA_InitStructure.DMA_Channel = DMA_Channel_4;
+	DMA_InitStructure.DMA_FIFOMode = DMA_FIFOMode_Disable;
+	DMA_InitStructure.DMA_FIFOThreshold = DMA_FIFOThreshold_HalfFull;
+	DMA_InitStructure.DMA_MemoryBurst = DMA_MemoryBurst_Single;
+	DMA_InitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
+
+	//setting circular mode
+	DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
+	//medium priority
+	DMA_InitStructure.DMA_Priority = DMA_Priority_High;
+	//source and destination data size word=32bit
+	DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+	DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
+	//automatic memory destination increment enable.
+	DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+	//source address increment disable
+	DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+	//Location assigned to peripheral register will be source
+	DMA_InitStructure.DMA_DIR = DMA_DIR_MemoryToPeripheral ;
+	//chunk of data to be transfered
+	DMA_InitStructure.DMA_BufferSize = ARRAYSIZE;
+	//source and destination start addresses
+	DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&USART2->DR;
+	DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t)&ADC_values[0]; //should still be the same from DMA2
+	//send values to DMA registers
+	DMA_Init(DMA1_Stream6, &DMA_InitStructure);
+	// Enable DMA2 Channel Transfer Complete interrupt
+	DMA_ITConfig(DMA1_Stream6, DMA_IT_TC, ENABLE);
+	DMA_Cmd(DMA1_Stream6, ENABLE);
+}
+
+void NVICInit(void){
 	NVIC_InitTypeDef NVIC_InitStructure;
 	//Enable DMA2 channel IRQ Channel */
 	NVIC_InitStructure.NVIC_IRQChannel = DMA2_Stream0_IRQn;
@@ -147,5 +206,36 @@ void DMAInit(void){
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_Init(&NVIC_InitStructure);
 
+	//Enable UART channel IRQ Channel */
+	NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+}
 
+void UARTInit(void){
+	RCC_APB2PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+	/**
+	 * Set Baudrate to value you pass to function
+	 * Disable Hardware Flow control
+	 * Set Mode To TX and RX, so USART will work in full-duplex mode
+	 * Disable parity bit
+	 * Set 1 stop bit
+	 * Set Data bits to 8
+	 *
+	 * Initialize USART2
+	 * Activate USART2
+	 */
+	USART_InitTypeDef USART_InitStruct;
+
+	USART_InitStruct.USART_BaudRate = BaudRate;
+	USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+	USART_InitStruct.USART_Parity = USART_Parity_No;
+	USART_InitStruct.USART_StopBits = USART_StopBits_1;
+	USART_InitStruct.USART_WordLength = USART_WordLength_8b;
+	USART_Init(USART2, &USART_InitStruct);
+	USART_Cmd(USART2, ENABLE);
+	USART_DMACmd(USART2, USART_DMAReq_Tx, ENABLE);
 }
